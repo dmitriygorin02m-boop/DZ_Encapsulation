9d3510bca95b15a058706a6d3414a0ba7801ea5f
Dmitriy Groin <dmitriy.gorin.02m@gmail.com>
2026-09-09 13:09:55 +0300

    Add flexible radio

--- src/test/java/ru/netology/stats/RadioTest.java
+++ src/test/java/ru/netology/stats/RadioTest.java
@@ -9,160 +9,108 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class RadioTest {
 
 
-    @Order(1)
+    @Order(1)       //==== Выставляем текущую радиостанцию =====
     @ParameterizedTest
     @CsvSource({
-            "-1,0", "0,0", "1,1", "5,5", "8,8", "9,9", "10,0"
+            "-1,0", "0,0", "1,1", "5,5", "8,8", "9,9", "10,9"
     })
-
     public void WeSetTheCurrentRadioStation(int setCurrentRadioStation, int expected) {
-
-        if (setCurrentRadioStation == -1) {
-            System.out.println();
-            System.out.println("==== Выставляем текущую радиостанцию =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Выставляем", "Ожидаемый результат", "Фактический результат");
-        }
 
         Radio cond = new Radio();
         cond.setCurrentRadioStation(setCurrentRadioStation);
-
         int actual = cond.getCurrentRadioStation();
-        System.out.printf("%-10d | %-20d | %-20d |%n", setCurrentRadioStation, expected, actual);
-        System.out.println();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Order(2)           //==== Выставляем текущую радиостанцию используя параметры Min/Max=====
+    @ParameterizedTest
+    @CsvSource({
+            "9,10", "10,10", "11,11", "34,34", "49,49", "50,50", "51,50"
+    })
+    public void WeSetTheCurrentRadioStationWithTheParameters(int setCurrentRadioStation, int expected) {
+
+        Radio cond = new Radio(10, 50);
+        cond.setCurrentRadioStation(setCurrentRadioStation);
+        int actual = cond.getCurrentRadioStation();
 
         Assertions.assertEquals(expected, actual);
     }
 
 
-    @Order(2)
+    @Order(3)           //===== Переключение радиостанций на следующую =====
     @ParameterizedTest
     @CsvSource({
             "0,1", "1,2", "8,9", "9,0"
     })
     public void SwitchToTheNextRadioStation(int setCurrentRadioStation, int expected) {
 
-        if (setCurrentRadioStation == 0) {
-            System.out.println();
-            System.out.println(
-                    "===== Переключение радиостанций на следующую =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Текущая", "Ожидаемый результат", "Фактический результат");
-        }
-
         Radio cond = new Radio();
-
         cond.setCurrentRadioStation(setCurrentRadioStation);
         cond.setNextRadioStation();
-
         int actual = cond.getCurrentRadioStation();
-        System.out.println();
 
-        System.out.printf("%-10d | %-20d | %-20d |%n", setCurrentRadioStation, expected, actual);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Order(3)
+    @Order(4)           //===== Переключение радиостанции на предыдущую =====
     @ParameterizedTest
     @CsvSource({
             "0,9", "1,0", "5,4", "9,8"
     })
     public void SwitchingTheRadioToThePreviousStation(int setCurrentRadioStation, int expected) {
-        if (setCurrentRadioStation == 0) {
-            System.out.println();
-            System.out.println("===== Переключение радиостанции на предыдущую =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Текущая", "Ожидаемый результат", "Фактический результат");
-        }
+
         Radio cond = new Radio();
         cond.setCurrentRadioStation(setCurrentRadioStation);
         cond.setRewRadioStation();
         int actual = cond.getCurrentRadioStation();
 
-        System.out.println();
-        System.out.printf("%-10d | %-20d | %-20d |%n", setCurrentRadioStation, expected, actual);
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Order(4)
+
+    @Order(5)           //===== Выставляем текущую громкость звука =====
     @ParameterizedTest
     @CsvSource({
-            "-1,0", "0,0", "1,1", "37,37", "99,99", "100,100", "101,0"
+            "-1,0", "0,0", "1,1", "37,37", "99,99", "100,100", "101,100"
     })
-    public void WeAdjustTheSoundVolume(int setSoundVolume, int expected) {
-        if (setSoundVolume == -1) {
-            System.out.println();
-            System.out.println("===== Выставляем текущую громкость звука =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Выставляем", "Ожидаемый результат", "Фактический результат");
-        }
+    public void WeAdjustTheSoundVolumeWithTheParameters(int setSoundVolume, int expected) {
+
         Radio cond = new Radio();
-
         cond.setSoundVolume(setSoundVolume);
-
         int actual = cond.getSoundVolume();
-
-        System.out.println();
-        System.out.printf("%-10d | %-20d | %-20d |%n", setSoundVolume, expected, actual);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Order(5)
+    @Order(6)               //===== Прибавляем громкость звука =====
     @ParameterizedTest
     @CsvSource({
             "0,1", "1,2", "44,45", "99,100", "100,100"
     })
     public void IncreaseTheSoundVolume(int setSoundVolume, int expected) {
-        if (setSoundVolume == 0) {
-            System.out.println();
-            System.out.println("===== Прибавляем громкость звука =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Текущая", "Ожидаемый результат", "Фактический результат");
-        }
 
         Radio cond = new Radio();
-
         cond.setSoundVolume(setSoundVolume);
         cond.setIncreaseSoundVolume();
-
         int actual = cond.getSoundVolume();
-
-        System.out.println();
-        System.out.printf("%-10d | %-20d | %-20d |%n", setSoundVolume, expected, actual);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Order(6)
+    @Order(8)               //===== Убавляем громкость звука =====
     @ParameterizedTest
     @CsvSource({
             "0,0", "1,0", "57,56", "99,98", "100,99"
     })
     public void DecreaseTheSoundVolume(int setSoundVolume, int expected) {
-        if (setSoundVolume == 0) {
-            System.out.println();
-            System.out.println("===== Убавляем громкость звука =====");
-            System.out.println();
-            System.out.printf(
-                    "%-10s | %-20s | %-20s |%n", "Текущая", "Ожидаемый результат", "Фактический результат");
-        }
+
         Radio cond = new Radio();
         cond.setSoundVolume(setSoundVolume);
         cond.setDecreasingVolume();
         int actual = cond.getSoundVolume();
 
-        System.out.println();
-        System.out.printf("%-10d | %-20d | %-20d |%n", setSoundVolume, expected, actual);
-
         Assertions.assertEquals(expected, actual);
-
     }
 }
